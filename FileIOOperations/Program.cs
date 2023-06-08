5610b231b5d2965219a9420cff35eb469e06a31e
Pankaj-Thorat <thoratpankaj282@gmail.com>
2023-06-08 23:46:27 +0530

    Check if file is present or not

---
+++ FileIOOperations/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace FileIOOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileExists();
+        }
+        public static void FileExists()
+        {
+            string path = @"C:\Users\thora\source\repos\FileIOOperations\FileIOOperations\Example.txt";
+            if (File.Exists(path))
+            {
+                Console.WriteLine("File Exists.");
+            }
+            else
+            {
+                Console.WriteLine("File does not exist.");
+            }
+        }
+        
+    }
+}
